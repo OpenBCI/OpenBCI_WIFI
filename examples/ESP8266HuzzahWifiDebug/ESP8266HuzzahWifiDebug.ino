29eaff98bbe9bd02b47b7328808153d38a4bebac
AJ Keller <pushtheworldllc@gmail.com>
2017-01-25 10:17:00 -0500

    Wifi udp working somewhat

--- examples/ESP8266HuzzahWifiDebug/ESP8266HuzzahWifiDebug.ino
+++ examples/ESP8266HuzzahWifiDebug/ESP8266HuzzahWifiDebug.ino
@@ -30,11 +30,10 @@ IPAddress client;
 // Create an instance of the server
 // specify the port to listen on as an argument
 // WiFiServer server(80);
-//
-// WiFiClient client;
 
-// WiFiServer server(80);
+// WiFiClient client;
 unsigned int localUdpPort = 2390;
+// WiFiServer server(80);
 
 volatile uint8_t packetCount = 0;
 volatile uint8_t maxPacketsPerWrite = 10;
@@ -81,54 +80,25 @@ void setup() {
   wifiManager.autoConnect("OpenBCI");
   printWifiStatus();
 
-  // data has been received from the master. Beware that len is always 32
-  // and the buffer is autofilled with zeroes if data is less than 32 bytes long
-  // It's up to the user to implement protocol for handling data length
-  SPISlave.onData([](uint8_t * data, size_t len) {
-    if (head >= maxPackets) {
-      head = 0;
-    }
+  Serial.println("Connected");
 
-    for (int i = 0; i < len; i++) {
-      ringBuf[head][i] = data[i];
-      head++;
-    }
-  });
+  // Start the server
+  // server.begin();
+  // Serial.println("Server started");
 
-  // The master has read out outgoing data buffer
-  // that buffer can be set with SPISlave.setData
-  SPISlave.onDataSent([]() {
-    // Serial.println("Answer Sent");
-  });
-
-  // status has been received from the master.
-  // The status register is a special register that bot the slave and the master can write to and read from.
-  // Can be used to exchange small data or status information
-  SPISlave.onStatus([](uint32_t data) {
-    // Serial.printf("Status: %u\n", data);
-    // SPISlave.setStatus(millis()); //set next status
-  });
-
-  // The master has read the status register
-  SPISlave.onStatusSent([]() {
-    // Serial.println("Status Sent");
-  });
-
-  // Setup SPI Slave registers and pins
-  SPISlave.begin();
-
-  // Set the status register (if the master reads it, it will read this value)
-  // SPISlave.setStatus(millis());
-
-  // Sets the data registers. Limited to 32 bytes at a time.
-  // SPISlave.setData(uint8_t * data, size_t len); is also available with the same limitation
-  // SPISlave.setData("Ask me a question!");
-
-  Serial.println("SPI Slave ready");
+  // Print the IP address
+  // Serial.println(WiFi.localIP());
 
 }
 
 void loop() {
+  // flushBuffer();
+  // // Check if a client has connected
+  // WiFiClient client_temp = server.available();
+  // if (!client_temp) {
+  //   return;
+  // }
+
   int packetSize = Udp.parsePacket();
   if (packetSize) {
     if (!clientSet) {
@@ -144,8 +114,11 @@ void loop() {
   if (clientSet) {
     tryFlushBuffer();
   }
-
-
+  if (head >= maxPackets) {
+    head = 0;
+  } else {
+    head++;
+  }
 }
 
 void flushBufferToUDP() {
