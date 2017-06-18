d39111d7980a69b7866e3c6ae474ec41eb46dd44
AJ Keller <pushtheworldllc@gmail.com>
2017-06-17 21:20:59 -0400

    Update ArduinoJSON to 5.10.1 and fix some issues with floats

--- examples/ESP8266HuzzahSSDP/ESP8266HuzzahSSDP.ino
+++ examples/ESP8266HuzzahSSDP/ESP8266HuzzahSSDP.ino
@@ -22,12 +22,12 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266SSDP.h>
 #include <WiFiManager.h>
-#include <ArduinoJson.h>
 #include "SPISlave.h"
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 // ENUMS
 typedef enum OUTPUT_MODE {
