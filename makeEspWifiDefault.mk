f084e35ddf0e7f3f7f61afafafec98d98c9e49af
jnaulty <jnaulty@gmail.com>
2017-11-16 00:07:07 -0800

    reformat layout and rename arduino libs variable

--- makeEspWifiDefault.mk
+++ makeEspWifiDefault.mk
@@ -1,7 +1,7 @@
 # My makefile
-LIBARAIES_DIR = $(HOME)/Arduino/libraries
-SKETCH = $(LIBARAIES_DIR)/OpenBCI_WIFI/examples/DefaultWifiShield/DefaultWifiShield.ino
-OPENBCI_WIFI_DIR = $(LIBARAIES_DIR)/OpenBCI_WIFI
+DUINO_LIBS_DIR = $(HOME)/Arduino/libraries
+SKETCH = $(DUINO_LIBS_DIR)/OpenBCI_WIFI/examples/DefaultWifiShield/DefaultWifiShield.ino
+OPENBCI_WIFI_DIR = $(DUINO_LIBS_DIR)/OpenBCI_WIFI
 
 UPLOAD_PORT = /dev/ttyUSB0
 ESP_ROOT = $(HOME)/esp8266
@@ -14,7 +14,31 @@ BOARD = huzzah
 FLASH_DEF = 4M1M
 # F_CPU = 160000000l
 
-LIBS = $(LIBARAIES_DIR)/OpenBCI_WIFI $(ESP_LIBS)/Wire $(ESP_LIBS)/GDBStub $(ESP_LIBS)/ESP8266WiFi  $(ESP_LIBS)/DNSServer $(ESP_LIBS)/ESP8266SSDP $(ESP_LIBS)/ESP8266WebServer $(ESP_LIBS)/ESP8266HTTPClient $(LIBARAIES_DIR)/WiFiManager $(ESP_LIBS)/SPISlave $(LIBARAIES_DIR)/ArduinoJson $(ESP_LIBS)/ESP8266mDNS $(ESP_LIBS)/ESP8266HTTPUpdateServer $(ESP_LIBS)/ArduinoOTA $(LIBARAIES_DIR)/PubSubClient $(LIBARAIES_DIR)/Time $(LIBARAIES_DIR)/NTPClient
-EXCLUDE_DIRS = $(LIBARAIES_DIR)/ArduinoJson/test $(LIBARAIES_DIR)/ArduinoJson/third-party $(LIBARAIES_DIR)/ArduinoJson/fuzzing $(LIBARAIES_DIR)/WebSockets/examples $(LIBARAIES_DIR)/PubSubClient/tests $(LIBARAIES_DIR)/OpenBCI_WIFI/test $(LIBARAIES_DIR)/OpenBCI_WIFI/tests-ptw-assert $(LIBARAIES_DIR)/OpenBCI_WIFI/extras
+LIBS = $(DUINO_LIBS_DIR)/OpenBCI_WIFI \
+       $(DUINO_LIBS_DIR)/WiFiManager \
+       $(DUINO_LIBS_DIR)/ArduinoJson \
+       $(DUINO_LIBS_DIR)/PubSubClient \
+       $(DUINO_LIBS_DIR)/Time \
+       $(DUINO_LIBS_DIR)/NTPClient \
+       $(ESP_LIBS)/Wire \
+       $(ESP_LIBS)/GDBStub \
+       $(ESP_LIBS)/ESP8266WiFi  \
+       $(ESP_LIBS)/DNSServer \
+       $(ESP_LIBS)/ESP8266SSDP \
+       $(ESP_LIBS)/ESP8266WebServer \
+       $(ESP_LIBS)/ESP8266HTTPClient \
+       $(ESP_LIBS)/SPISlave \
+       $(ESP_LIBS)/ESP8266mDNS \
+       $(ESP_LIBS)/ESP8266HTTPUpdateServer \
+       $(ESP_LIBS)/ArduinoOTA 
+
+EXCLUDE_DIRS = $(DUINO_LIBS_DIR)/ArduinoJson/test \
+	       $(DUINO_LIBS_DIR)/ArduinoJson/third-party \
+	       $(DUINO_LIBS_DIR)/ArduinoJson/fuzzing \
+	       $(DUINO_LIBS_DIR)/WebSockets/examples \
+	       $(DUINO_LIBS_DIR)/PubSubClient/tests \
+	       $(DUINO_LIBS_DIR)/OpenBCI_WIFI/test \
+	       $(DUINO_LIBS_DIR)/OpenBCI_WIFI/tests-ptw-assert \
+	       $(DUINO_LIBS_DIR)/OpenBCI_WIFI/extras
 
 include $(HOME)/makeEspArduino/makeEspArduino.mk
